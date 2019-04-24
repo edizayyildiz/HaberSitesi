fe34a9dd86dbd438cd2d19b857b7b99a12afe718
edizayyildiz <ayyildizediz@gmail.com>
2019-04-24 23:01:52 +0300

    Service katmanı şimdilik hazır.

---
+++ HaberSitesi.Service/CategoryService.cs
@@ -0,0 +1,78 @@
+﻿using HaberSitesi.Data;
+using HaberSitesi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaberSitesi.Service
+{
+    public interface ICategoryService
+    {
+        void Insert(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+        void Delete(Guid id);
+        Category Find(Guid id);
+        IEnumerable<Category> GetAll();
+        IEnumerable<Category> GetAll(Expression<Func<Category, bool>> where);
+    }
+
+    public class CategoryService : ICategoryService
+    {
+        private readonly IRepository<Category> categoryRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public CategoryService(IRepository<Category> categoryRepository, IUnitOfWork unitOfWork)
+        {
+            this.categoryRepository = categoryRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Delete(Category category)
+        {
+            categoryRepository.Delete(category);
+            unitOfWork.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            var categoryToDelete = this.Find(id);
+            if (categoryToDelete != null)
+            {
+                this.Delete(categoryToDelete);
+            }
+        }
+
+        public Category Find(Guid id)
+        {
+            return categoryRepository.Find(id);
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return categoryRepository.GetAll();
+        }
+
+        public IEnumerable<Category> GetAll(Expression<Func<Category, bool>> where)
+        {
+            return categoryRepository.GetAll(where);
+        }
+
+        public void Insert(Category category)
+        {
+            categoryRepository.Insert(category);
+            unitOfWork.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            var model = this.Find(category.Id);
+            model.Name = category.Name;
+            model.Description = category.Description;
+            categoryRepository.Update(model);
+            unitOfWork.SaveChanges();
+        }
+    }
+}
---
+++ HaberSitesi.Service/PostService.cs
@@ -0,0 +1,80 @@
+﻿using HaberSitesi.Data;
+using HaberSitesi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaberSitesi.Service
+{
+    public interface IPostService
+    {
+        void Insert(Post post);
+        void Update(Post post);
+        void Delete(Post post);
+        void Delete(Guid id);
+        Post Find(Guid id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetAll(Expression<Func<Post, bool>> where);
+    }
+
+    public class PostService : IPostService
+    {
+        private readonly IRepository<Post> postRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public PostService(IRepository<Post> postRepository, IUnitOfWork unitOfWork)
+        {
+            this.postRepository = postRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Delete(Post post)
+        {
+            postRepository.Delete(post);
+            unitOfWork.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            var postToDelete = this.Find(id);
+            if (postRepository != null)
+            {
+                this.Delete(postToDelete);
+            }
+        }
+
+        public Post Find(Guid id)
+        {
+            return postRepository.Find(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return postRepository.GetAll();
+        }
+
+        public IEnumerable<Post> GetAll(Expression<Func<Post, bool>> where)
+        {
+            return postRepository.GetAll(where);
+        }
+
+        public void Insert(Post post)
+        {
+            postRepository.Insert(post);
+            unitOfWork.SaveChanges();
+        }
+
+        public void Update(Post post)
+        {
+            var model = this.Find(post.Id);
+            model.Title = post.Title;
+            model.Description = post.Description;
+            model.Body = post.Body;
+            model.CategoryId = post.CategoryId;
+            postRepository.Update(model);
+            unitOfWork.SaveChanges();
+        }
+    }
+}
